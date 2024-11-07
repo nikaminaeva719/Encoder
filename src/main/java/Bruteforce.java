67cab52f283cbd96e7d10eb20a982a75e03c216d
nikaminaeva719 <meraylatin@yandex.ru>
2024-11-07 21:28:49 +0300

    Был произведен рефакторинг кода

--- src/main/java/Bruteforce.java
+++ src/main/java/Bruteforce.java
@@ -1,55 +1,35 @@
 import lombok.SneakyThrows;
 
-import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Bruteforce {
+
+    private static final int MAX_WORD_LENGTH = 28;
 
     @SneakyThrows
     public void bruteforce() {
         ConsoleHelper.writeMessage("Введите путь к файлу:");
         String src = ConsoleHelper.readString();
         Path dst = ConsoleHelper.buildFileName(src, "_bruteforce");
-        CaesarCipher caesarCipher = new CaesarCipher();
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get(src));
-             BufferedWriter writer = Files.newBufferedWriter(dst)) {
-            int length = caesarCipher.alphabetLength();
-            StringBuilder builder = new StringBuilder();
-            List<String> list = new ArrayList<>(); //исходный, до
-            while (reader.ready()) {
-                String line = reader.readLine();
-                builder.append(line).append(System.lineSeparator()); //универсальный перенос строки
-                list.add(line);
-            }
-            for (int i = 0; i < length; i++) {
-                String result = caesarCipher.decrypt(builder.toString(), i);
-                if (isValidate(result)) {
-                    for (String line : list) {
-                        String decrypt = caesarCipher.decrypt(line, i);
-                        writer.write(decrypt);
-                        writer.newLine();
-                    }
-
-                    ConsoleHelper.writeMessage("Результат успешно записан в " + dst);
-                    ConsoleHelper.writeMessage("Ключе шифрования: " + i);
-                    break;
-                }
-
+        CaesarCipher caesar = new CaesarCipher();
+        String content = Files.readString(Paths.get(src));
+        for (int i = 0; i < caesar.alphabetLength(); i++) {
+            String decrypt = caesar.decrypt(content, i);
+            if (isValidate(decrypt)) {
+                Files.writeString(dst, decrypt);
+                ConsoleHelper.writeMessage("Результат успешно записан в " + dst);
+                ConsoleHelper.writeMessage("Ключе шифрования: " + i);
+                break;
             }
         }
     }
 
-
     private boolean isValidate(String text) {
-        String[] words = text.split(" ");
         boolean isValidate = false;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].length() > 28) {
+        for (String word : text.split(" ")) {
+            if (word.length() > MAX_WORD_LENGTH) {
                 return false;
             }
         }
